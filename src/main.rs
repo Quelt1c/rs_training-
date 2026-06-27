76c642deac29f8712cf92bf67d23f0a39b7b7690
Quelt1c <lalkalalkovich953@gmail.com>
2026-06-27 13:13:04 +0300

    make own split and rewrite some syntax errors

--- src/main.rs
+++ src/main.rs
@@ -33,12 +33,41 @@ fn split_by_word(map: &mut HashMap<String, Vec<usize>>, text: &str)
     }
 }
 
-fn main() {
-    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel nibh massa. Duis convallis elementum felis sit amet elementum. Donec vitae nibh sed lorem luctus consectetur. Fusce aliquam, eros sit amet elementum suscipit, lorem lorem ultricies nibh, sed malesuada augue orci nec dui. Phasellus gravida dolor in nunc pulvinar suscipit. Pellentesque nec molestie tellus. Pellentesque ut diam quis arcu sodales bibendum nec ut lorem. Suspendisse sagittis lacus metus, ut vestibulum quam lacinia a. Ut ac diam eu metus finibus pharetra eu quis erat. Phasellus cursus luctus tempus.";
-    //let count = HashMap::new();
-    //position_indices(&count);
-    let mut count_word = HashMap::new();
-    split_by_word(&mut count_word, text);
-    print_string(&count_word);
+fn split_by_word_own(map: &mut HashMap<String, Vec<usize>>, text: &str)
+{
+    let mut current_word = String::new();
+    let mut start_index: Option<usize> = None;
 
+    for(i, c) in text.char_indices()
+    {
+        if c.is_alphanumeric()
+        {   
+            if start_index.is_none()
+            {
+                start_index = Some(i);
+            }
+            current_word.push(c);
+        }
+        else
+        {
+            if let Some(ind) = start_index 
+            {
+                map.entry(current_word.clone()).or_insert(Vec::new()).push(ind);
+                current_word.clear();
+                start_index = None;
+            }
+        }
+    }
+    if let Some(ind) = start_index
+    {
+        map.entry(current_word).or_insert(Vec::new()).push(ind);
+    }
+}
+
+fn main() {
+    let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    let text_ukr = "Україна... В одному вже тільки цьому слові і для нашого вуха, і для вуха чужинців бринить ціла музика смутку і жалю... Україна — країна смутку і краси, країна, де найбільше люблять волю... Україна — це тихі води і ясні зорі, зелені сади, білі хати, лани золотої пшениці, медові та молочні ріки... Україна — розкішний вінок із рути і барвінку, що над ним світять заплакані золоті зорі... Поема жалю і смутку, краси і недолі...";
+    let mut count = HashMap::new();
+    split_by_word_own(&mut count, text_ukr);
+    print_string(&count);
 }
