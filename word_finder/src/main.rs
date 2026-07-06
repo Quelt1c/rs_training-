ff05fb830dc77aa092dc5a1a1448bf61cf1ced22
Quelt1c <lalkalalkovich953@gmail.com>
2026-07-06 07:05:58 +0300

    add threads functionality

--- word_finder/src/main.rs
+++ word_finder/src/main.rs
@@ -1,18 +1,36 @@
 use crate::case_checker::Checker;
 use clap::Parser;
 use std::collections::HashMap;
-use std::path::PathBuf;
+use std::sync::{Arc, Mutex};
 
 mod case_checker;
 mod io_utils;
 mod text_tools;
+mod threads_create;
 
 fn main() -> std::io::Result<()> {
     let args = Checker::parse();
 
-    let mut map = HashMap::<String, HashMap<PathBuf, Vec<usize>>>::new();
+    let shared_map = Arc::new(Mutex::new(HashMap::new()));
 
-    io_utils::walk_dir(&args.file_path, &mut map, args.case_sensitive)?;
+    let handles = threads_create::spawn_search_threads(
+        &args.file_path,
+        Arc::clone(&shared_map),
+        args.case_sensitive,
+    )?;
+
+    println!("Threads are launched: {}.", handles.len());
+
+    for handle in handles {
+        handle
+            .join()
+            .expect("One of the threads finished with an error");
+    }
+
+    let map = Arc::try_unwrap(shared_map)
+        .expect("Error: Arc is still being held by some thread")
+        .into_inner()
+        .expect("Error: Mutex is in a poisoned state");
 
     let search_word = if args.case_sensitive {
         args.text
